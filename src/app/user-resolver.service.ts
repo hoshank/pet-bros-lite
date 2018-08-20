1d9f559026e45c767167c1c8fb4e73593fe8c9d8
sis0k0 <stanimira.vlaeva@gmail.com>
2018-08-20 14:29:59 +0300

    refactor: remove unused rxjs imports

--- src/app/user-resolver.service.ts
+++ src/app/user-resolver.service.ts
@@ -1,27 +1,20 @@
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/take';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
 import {
   Router,
   Resolve,
-  RouterStateSnapshot,
-  ActivatedRouteSnapshot,
 } from '@angular/router';
 
 import { UserService } from './user.service';
-import { take, map } from 'rxjs/operators';
 
 @Injectable()
 export class UserResolver implements Resolve<void> {
   constructor(private userService: UserService, private router: Router) {
   }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-    return this.userService.user$.pipe(
-      take(1),
-      map(user => user)
-    );
+  resolve(): Observable<any> {
+    return this.userService.user$.pipe(take(1));
   }
 }
 
