b744a2d4c0d5c2d9899d06b02ee72a1a300c5295
sebawita <sebawita@gmail.com>
2018-08-20 16:52:08 +0100

    add missing provideIn

--- src/app/user-resolver.service.ts
+++ src/app/user-resolver.service.ts
@@ -8,7 +8,7 @@ import {
 
 import { UserService } from './user.service';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class UserResolver implements Resolve<void> {
   constructor(private userService: UserService, private router: Router) {
   }
